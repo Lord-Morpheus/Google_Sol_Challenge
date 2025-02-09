b3fb15aed4451382cc05e95ac603af5668af9843
sharmagopesh21 <sharmagopesh250@gmail.com>
2025-02-09 09:04:36 +0530

    Add book obect created

--- Frontend/src/pages/Addbook.jsx
+++ Frontend/src/pages/Addbook.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const Addbook = () => {
+  const [book, setBook] = useState({
+    bookName: "",
+    authorName: "",
+    genre: "",
+    language: "",
+    publication: "",
+    publishedYear: "",
+    age: "",
+    description: "",
+  });
+  const handlesubmit = (e) => {
+    console.log(book);
+    e.preventDefault();  
+  };
   return (
     <div className="grid place-items-center bg-gray-100 rounded-xl h-full p-5">
       <h1 className="font-bold text-3xl">Add Book</h1>
@@ -10,21 +25,29 @@ const Addbook = () => {
             type="text"
             placeholder="Book Name"
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"
+            value={book.bookName}
+            onChange={(e) => setBook({ ...book, bookName: e.target.value })}
           />
           <input
             type="text"
             placeholder="Author Name"
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"
+            value={book.authorName} 
+            onChange={(e) => setBook({ ...book, authorName: e.target.value })}
           />
         </div>
         <div className="flex w-full">
           <input
             type="text"
             placeholder="Genre"
+            value={book.genre}
+            onChange={(e) => setBook({ ...book, genre: e.target.value })}
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"/>
           <input
             type="text"
             placeholder="Language"
+            value={book.language}
+            onChange={(e) => setBook({ ...book, language: e.target.value })}
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"
           />
         </div>
@@ -32,11 +55,15 @@ const Addbook = () => {
           <input
             type="text"
             placeholder="Publication"
+            value={book.publication}  
+            onChange={(e) => setBook({ ...book, publication: e.target.value })}
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"
           />
           <input
             type="text"
             placeholder="Published Year"
+            value={book.publishedYear}
+            onChange={(e) => setBook({ ...book, publishedYear: e.target.value })}
             className="p-2 m-2 border-2 border-gray-300 rounded-md w-full"
           />
         </div>
@@ -64,12 +91,16 @@ const Addbook = () => {
         </div>
         <textarea
           type="text"
+          value={book.description}
+          onChange={(e) => setBook({ ...book, description: e.target.value })}
           placeholder="Description"
           className="p-2 m-2 border-2 border-gray-300 rounded-md w-full min-h-32"
         />
         <input
           type="text"
           placeholder="Minimum Age"
+          value={book.age}
+          onChange={(e) => setBook({ ...book, age: e.target.value })}
           className=" self-center p-2 m-2 border-2 border-gray-300 rounded-md w-32"
         />
         <button
